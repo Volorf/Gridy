a3e5baf2e8f5a977265808b09bb82c2b6a2f4789
Oleg Frolov <frolololeg@gmail.com>
2017-04-05 17:13:26 +0300

    Did it

--- gridy.sketchplugin/Contents/Sketch/gridy.js
+++ gridy.sketchplugin/Contents/Sketch/gridy.js
@@ -6,6 +6,10 @@ function createHorizontalGuides(context) {
 	superMainFunc(context, "horizontal")
 }
 
+function removeAllGuides(context) {
+	superMainFunc(context, "remove")
+}
+
 function superMainFunc (context, whatWeDo) {
 
 	var doc = context.document,
@@ -14,8 +18,12 @@ function superMainFunc (context, whatWeDo) {
 		fontSizeSelectedTextLayer = 0,
 	  sketch = context.api(),
 		layerWidth = layer.frame().width(),
+		layerHeight = layer.frame().height(),
 		layerXMin = layer.frame().x(),
-		layerXMax = layer.frame().x() + layerWidth;
+		layerYMin = layer.frame().y(),
+		layerXMax = layerXMin + layerWidth,
+		layerYMax = layerYMin + layerHeight,
+		target = [[doc currentPage] currentArtboard] || [doc currentPage];
 
 		if (selection.length == 0) {
 			doc.showMessage("You should select at least one text layer");
@@ -36,7 +44,6 @@ function superMainFunc (context, whatWeDo) {
 		}
 
 	function createVeticalGuides () {
-		var target = [[doc currentPage] currentArtboard] || [doc currentPage];
 		var input = Number(doc.askForUserInput_initialValue("Amount of sections", "1"));
 		var unit = layerWidth / input
 		[[target horizontalRulerData] addGuideWithValue:layerXMin]
@@ -46,14 +53,34 @@ function superMainFunc (context, whatWeDo) {
 				[[target horizontalRulerData] addGuideWithValue: layerXMin + (unit * i)]
 			}
 		}
+		doc.showMessage("Vertical guidelines have been created")
 	}
 
 	function createHorizontalGuides () {
-
+		var input = Number(doc.askForUserInput_initialValue("Amount of sections", "1"));
+		var unit = layerHeight / input
+		[[target verticalRulerData] addGuideWithValue:layerYMin]
+		[[target verticalRulerData] addGuideWithValue:layerYMax]
+		if (input > 1) {
+			for (var i = 1; i < input; i++) {
+				[[target verticalRulerData] addGuideWithValue: layerYMin + (unit * i)]
+			}
+		}
+		doc.showMessage("Horizontal guidelines have been created");
 	}
 
 	function removeAllGuides () {
-
+		var countVertical = [[target verticalRulerData] numberOfGuides]
+		var countHorizontal = [[target horizontalRulerData] numberOfGuides]
+		while(countVertical > 0 ) {
+    	[[target verticalRulerData] removeGuideAtIndex:0]
+    	var countVertical = [[target verticalRulerData] numberOfGuides]
+		}
+		while(countHorizontal > 0 ) {
+    	[[target horizontalRulerData] removeGuideAtIndex:0]
+    	var countHorizontal = [[target horizontalRulerData] numberOfGuides]
+		}
+		doc.showMessage("Guidelines have been removed");
 	}
 
 }
